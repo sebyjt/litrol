3933c3bd7404b8d57dffa53a6710c65e5bfdf49d
seby <sebyjoseph@cs.ajce.in>
2018-07-09 18:25:11 +0530

    API integrated

--- litrol/lib/FirstPage.dart
+++ litrol/lib/FirstPage.dart
@@ -2,14 +2,56 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:async';
 import 'dart:convert' show json, utf8;
+import 'package:http/http.dart' as http;
 
-class First extends StatelessWidget {
-  
+class First extends StatefulWidget {
+  @override
+  first createState()=>new first();
+  }
+class first extends State<First>
+{
+  String Price=" ";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPrice();
+
+  }
+  Future getPrice()
+  async {
+
+    String url =
+        'https://litrol.herokuapp.com/getprice';
+
+
+    Future<String> apiRequest(String url) async {
+
+
+      var response = await http.get(
+          url,
+          headers:{ "Content-Type":"application/json" }
+      );
+
+      print(response.body);
+
+      Map map=json.decode(response.body);
+
+      setState(() {
+
+       Price=map["Price"];
+      });
+
+      return "success";
+
+    }
+    await apiRequest(url);
+  }
   @override
   Widget build(BuildContext context) {
     return new Container(
       child: new Center(
-        child: new Text('Todays price is ₹81.0',
+        child: new Text('Todays price is '+Price,
         style: new TextStyle(
               
               fontSize: 25.0,
--- litrol/lib/SecondPage.dart
+++ litrol/lib/SecondPage.dart
@@ -1,11 +1,58 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class Second extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class Second extends StatefulWidget {
+  @override
+  second createState()=>new second();
+}
+class second extends State<Second>
+{
+  String Price=" ";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getPrice();
+
+  }
+  Future getPrice()
+  async {
+
+    String url =
+        'https://litrol.herokuapp.com/predict';
+
+
+    Future<String> apiRequest(String url) async {
+
+
+      var response = await http.get(
+          url,
+          headers:{ "Content-Type":"application/json" }
+      );
+
+      print(response.body);
+
+      Map map=json.decode(response.body);
+
+      setState(() {
+
+        Price=map["Price"];
+        Price=Price.substring(0,5);
+      });
+
+      return "success";
+
+    }
+    await apiRequest(url);
+  }
   @override
   Widget build(BuildContext context) {
     return new Container(
       child: new Center(
-        child: new Text('Tomorrows price is ₹82.1',
+        child: new Text('Tomorrows price is '+Price,
         style: new TextStyle(
               
               fontSize: 25.0,
